bf42f471cf4203582fe779641275be4818020cea
35kartal35 <mozdemir3545@gmail.com>
2023-03-08 21:14:51 +0300

    json-server get

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,16 +2,21 @@ import React from "react";
 import Header from "../component/header";
 import Footer from "../component/footer";
 import { useSelector } from "react-redux";
+
+
+
 const Home = () => {
     const appState = useSelector(state => state)
     console.log(appState);
     return (
-        <div>
-            <Header />
-
-            <Footer />
-        </div>
-
+        <>
+            <div className="conteiner">
+                <Header />
+                <h1>{appState.categoriesState.categories[1].name}</h1>
+                {appState.blogsState.blogs[0].title}
+                <Footer />
+            </div>
+        </>
     )
 }
 
